a79c618e536eab0eb2a8df7472d0c1616ec1778e
anatoly.kryzhanovsky <anatoly.kryzhanovsky@gmail.com>
2025-02-08 07:11:02 +0300

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/about">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          
         </div>
       </div>
     </header>
